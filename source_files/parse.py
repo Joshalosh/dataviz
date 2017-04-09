73000901da054126396d191d52c8196d8804e827
Josh Jessop-smith <joshjessop-smith@Joshs-MacBook-Pro.local>
2017-04-09 14:51:21 +1000

    finished the parse.py script

--- source_files/parse.py
+++ source_files/parse.py
@@ -6,11 +6,31 @@ def parse(raw_file, delimiter):
 	""" Passes a raw CSV file to a JSON-line object. """
 
 	# Open CSV file
+	opened_file = open(raw_file)
 
 	# Read CSV file
+	csv_data = csv.reader(opened_file, delimiter=delimiter)
 
-	# Close CSV file
+	# Setup an empty list
+	parsed_data = []
 
-	# Build a data structure to return parsed_data
+	# Skip over the first line of the file for the headers
+	fields = csv_data.next()
+
+	# Iterate over each row of the csv file, zip together field -> value
+	for row in csv_data:
+		parsed_data.append(dict(zip(fields, row)))
+
+	# Close the CSV file
 
 	return parsed_data
+
+def main():
+	# Call our parse function and give it the needed parameters
+	new_data = parse(MY_FILE, ",")
+
+	# Lets see what the data looks like
+	print(new_data)
+
+if __name__ = "__main__":
+	main()
