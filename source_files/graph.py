f93cc4abe5fe62f1fc91c8e72819f00a4cccb91a
Josh Jessop-smith <joshjessop-smith@Joshs-MacBook-Pro.local>
2017-04-16 15:18:38 +1000

    About to test Part 1 graph.py. I think there are python3 compatability issues to sort out

--- source_files/graph.py
+++ source_files/graph.py
@@ -23,65 +23,43 @@ def visualize_days():
     """Visualize data by day of week"""
     
     # Grab our parsed data that we parsed earlier
+    data_file = parse('MY_FILE', ",")
 
-    # Make a new varuable, 'counter', from iterating through each
+    # Make a new variable, 'counter', from iterating through each
     # line of data in the parsed data, and count how many incidents
     # happen on each day
+    counter = Counter(item['DayOfWeek'] for item in data_file)
 
     # Seperate the x-axis data (the days of the week) from the
     # 'counter' variable from the y-axis data (the number of
     # incidents for each day)
+    data_list = [
+                 counter['Monday'],
+                 counter['Tuesday'],
+                 counter['Wednesday'],
+                 counter['Thursday'],
+                 counter['Friday'],
+                 counter['Saturday'],
+                 counter['Sunday'],
+                 ]
+
+    day_tuple = (['Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'])
 
     # With that y-axis data, assign it to a matplotlib plot instance
-
+    plt.plot(data_list)
     # Create the amount of ticks needed for our x-axis, and assign
     # the labels
+    plt.xticks(range(len(day_tuple)), day_tuple)
 
     # Save the plot!
+    plt.savefig('Days.png')
 
     # Close plot file
-
-
-def visualize_type():
-    """Visualize data by category in a bar graph"""
-    data_file = parse(MY_FILE, ",")
-    # Same as before, this returns a dict where it sums the total
-    # incidents per Category.
-    counter = Counter(item["Category"] for item in data_file)
-
-    # Set the labels which are based on the keys of our counter.
-    labels = tuple(counter.keys())
-
-    # Set where the labels hit the x-axis
-    xlocations = np.arange(len(labels)) + 0.5
-
-    # Width of each bar
-    width = 0.5
-
-    # Assign data to a bar plot
-    plt.bar(xlocations, counter.values(), width=width)
-
-    # Assign labels and tick location to x-axis
-    plt.xticks(xlocations + width / 2, labels, rotation=90)
-
-    # Give some more room so the labels aren't cut off in the graph
-    plt.subplots_adjust(bottom=0.4)
-
-    # Make the overall graph/figure larger
-    plt.rcParams['figure.figsize'] = 12, 8
-
-    # Save the graph!
-    # If you look at new-coder/dataviz/tutorial_source, you should see
-    # the PNG file, "Type.png".  This is our graph!
-    plt.savefig("Type.png")
-
-    # Close figure
     plt.clf()
 
 
 def main():
     visualize_days()
-    visualize_type()
 
 
 if __name__ == "__main__":
