a4b020c4dd9b103adec35ef8dd4e66ac3105f1f5
Josh Jessop-smith <joshjessop-smith@Joshs-MacBook-Pro.local>
2017-04-16 10:43:55 +1000

    Beginning graph.py

--- source_files/parse.py
+++ source_files/parse.py
@@ -1,4 +1,5 @@
 import csv
+import parse
 
 MY_FILE = "../data/sample_sfpd_incident_all.csv"
 
@@ -26,9 +27,24 @@ def parse(raw_file, delimiter):
 
 	return parsed_data
 
+def copy_data(parsed_data):
+
+	# Open file in write mode
+	data_copy = open('info.txt', 'w')
+
+	# Write CSV information into file
+	for info in parse(MY_FILE, ','):
+		data_copy.write(info['Descript'])
+		data_copy.write('\n')
+
+
+	# Close file
+	data_copy.close()
+
 def main():
 	# Call our parse function and give it the needed parameters
 	new_data = parse(MY_FILE, ",")
+	copy_data(new_data)
 
 	# Lets see what the data looks like
 	print(new_data)
