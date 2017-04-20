41ce5b175c4366cb6914570969410c86f54ca2a2
Josh Jessop-smith <joshjessop-smith@Joshs-MBP.lan>
2017-04-20 18:28:05 +1000

    created visualise type function, which creates a bar graph

--- source_files/graph.py
+++ source_files/graph.py
@@ -19,7 +19,7 @@ import numpy as np
 MY_FILE = "../data/sample_sfpd_incident_all.csv"
 
 
-def visualise_days():
+def visualize_days():
     """Visualize data by day of week"""
     
     # Grab our parsed data that we parsed earlier
@@ -47,6 +47,7 @@ def visualise_days():
 
     # With that y-axis data, assign it to a matplotlib plot instance
     plt.plot(data_list)
+
     # Create the amount of ticks needed for our x-axis, and assign
     # the labels
     plt.xticks(range(len(day_tuple)), day_tuple)
@@ -73,24 +74,32 @@ def visualise_type():
 
     # Set exactly where the labels hit the x-axis
     xlocations = np.arange(len(labels)) + 0.5
-    
+
     # Width of each bar that will be plotted
+    width = 0.5
 
     # Assign data to bar plot (similar to plt.plot()!)
+    plt.bar(xlocations, counter.values(), width=width)
 
     # Assign labels and tick location to x-axis
+    plt.xticks(xlocations + width / 2, labels, rotation=90)
 
     # Give some more room so the x-axis labels aren't cut off in the
     # graph
+    plt.subplots_adjust(bottom=0.5)
 
     # Make the overall graph/figure larger
+    plt.rcParams['figure.figsize'] = 12, 10
 
     # Save the graph!
+    plt.savefig("Type.png")
 
     # Close plot figure
+    plt.clf()
 
 def main():
     visualize_days()
+    visualise_type()
 
 
 if __name__ == "__main__":
