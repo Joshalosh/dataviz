3267a14a923b6123478972d44689a5f3a2c2d4df
Josh Jessop-smith <joshjessop-smith@Joshs-MBP.lan>
2017-04-28 22:50:03 +1000

    Started filling in create_map()

--- source_files/map.py
+++ source_files/map.py
@@ -26,21 +26,35 @@ def create_map(data_file):
     """
 
     # Define type of GeoJSON we're creating
+    geo_map = {type: "FeatureCollection"}
 
     # Define empty list to collect each point to graph
+    item_list = []
 
     # Iterate over our data to create GeoJSON document.
     # We're using enumerate() so we get the line, as well
     # the index, which is the line number
+    for index, line in enumerate(data_file):
 
     	# Skip any zero coordinates as this will throw off
     	# our map
+        if line['X'] == 0 or line['Y'] == 0:
+            continue
 
     	# Setup a new dictionary for each iteration
+        data = {}
 
     	# Assign line items to appropriate GeoJSON fields
+        data['type'] = 'Feature'
+        data['id'] = index
+        data['Properties'] = {'title': line['Category'],
+                                'descripton': line['Descript'],
+                                'date': line['Date']}
+        data['geometry'] ={'type': 'Point',
+                            'coordinates': line['X'], line['Y']}
 
     	# Add data dictionary to our item_list
+        item_list.append(data)
 
     # For each point in our item_list, we add the point to our
     # dictionary. setdefault creates a key called 'features' that
